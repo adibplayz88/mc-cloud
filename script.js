a916df6602a437373294e0b92b9ecd643530123e
adibplayz88 <adibplayz88@gmail.com>
2026-02-14 21:21:31 +0530

    Script update

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function play(){
-    let name=document.getElementById("name").value;
-    if(!name){alert("Enter username");return;}
+    let name = document.getElementById("name").value;
+    if(!name){
+        alert("Enter username bro");
+        return;
+    }
 
-    document.getElementById("game").innerHTML=
-    `<iframe src="CLIENT_LINK_HERE" width="100%" height="600"></iframe>`;
+    document.getElementById("game").innerHTML =
+    `<iframe src="https://eaglercraft.com/mc/1.8/" 
+    width="100%" height="600" style="border:none;"></iframe>`;
 }
